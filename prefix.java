644e4141e9c49c7df777b6314e7e13819e20ff5f
dhivya-rece <dhivyarajendran1996@gmail.com>
2016-07-19 10:59:41 +0530

    fd
    f

---
+++ prefix.java
@@ -0,0 +1,22 @@
+class String{
+public Static void main()
+{
+String a="hai how hi";
+String[]b=a.split("");
+int c=b.length;
+for(int i=0;i<c;i++){
+char[] d=a[i].toCharArray();
+int e=d.length;
+for(int j=0;j<e;j++)
+{
+if(d[i]==d[i+1])
+{
+char v=d[i];
+}
+}
+
+}
+System.out.println(d[i]);
+}
+}
+
