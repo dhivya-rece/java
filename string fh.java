cd572717643cd97503d2146449d4788ea434edae
dhivya-rece <dhivyarajendran1996@gmail.com>
2016-07-23 14:57:37 +0530

    fgsd
    gfgf

---
+++ string fh.java
@@ -0,0 +1,19 @@
+class java
+{
+public static void main(String args[])
+{
+String a;
+int b=a.length;
+char[] h=a.tocharArray();
+for(int i=0;i<b-1;i++)
+{
+for(int j=1;j< b;j++)
+{
+if(h[i]==h[j])
+{
+system.out.println(h[i]);
+}
+}
+}
+}
+}
